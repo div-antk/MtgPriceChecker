b970cfb0b41fd62a4be611575f2a691e13777df9
div-antk <51283468+owaii@users.noreply.github.com>
2021-06-18 23:58:15 +0900

    Entity設定

---
+++ MtgPriceChecker/Entity/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  MtgPriceChecker
+//
+//  Created by Takuya Ando on 2021/06/17.
+//
+
+import Foundation
+
+struct CardResponse: Codable {
+    let data: [Data]
+}
+
+struct Data: Codable {
+    let name: String?
+    let prices: [Prices]
+    let image_uris: [Images]
+}
+
+struct Prices: Codable {
+    let usd: String?
+    let usd_foil: String?
+}
+
+struct Images: Codable {
+    let normal: URL
+}
