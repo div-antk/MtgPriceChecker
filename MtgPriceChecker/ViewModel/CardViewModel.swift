8bfabe7a5f3c8b63f0198a5e25271afd4e29601f
div-antk <51283468+owaii@users.noreply.github.com>
2021-06-22 22:57:10 +0900

    VM実装中

--- MtgPriceChecker/ViewModel/CardViewModel.swift
+++ MtgPriceChecker/ViewModel/CardViewModel.swift
@@ -19,30 +19,48 @@ protocol CardViewModelOutputs {
 }
 
 protocol CardViewModelType {
-    var input: CardViewModelInputs { get }
+    var input: CardViewModelInputs { get}
     var output: CardViewModelOutputs { get }
 }
 
-class CardViewModel: CardViewModelType, CardViewModelInputs, CardViewModelOutputs {
-    
-    var input: CardViewModelInputs
-    var output: CardViewModelOutputs
-    
+class CardViewModel: CardViewModelInputs, CardViewModelOutputs {
+
     // MARK: - INPUT
     let searchWord: AnyObserver<String>
     
     // MARK: - OUTPUT
     let cardData: Observable<[CardResponse]>
     
+    // MARK: - OTHER
     private let disposeBag = DisposeBag()
     private let apiProvider = MoyaProvider<MtgAPI>()
     
     init() {
+        // 流れてきたデータを cardDataに入れる
+        let _cardData = PublishRelay<[CardResponse]>()
+        self.cardData = _cardData.asObservable()
+        
+        
         let _searchWord = PublishRelay<String>()
         self.searchWord = AnyObserver<String>() { event in
             guard let text = event.element else { return }
             _searchWord.accept(text)
         }
+        
+        _searchWord
+            .debounce(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+            .flatMap { searchWord in
+                CardRepository.getCardData(name: searchWord)
+            }
+            .subscribe(onNext: { response in
+                _cardData.accept(response)
+            })
+            .disposed(by: disposeBag)
+        
     }
 }
 
+extension CardViewModel: CardViewModelType {
+    var input: CardViewModelInputs { return self }
+    var output: CardViewModelOutputs { return self }
+}
